96a85813846e489daad0d87f2daf151b72c78973
Amit <tiwamit44@gmail.com>
2022-08-31 14:05:51 +0530

    Adding @LoadBalanced  to add Application name is url(No need to pass localhost/port)

--- Order-Service/src/main/java/com/example/amit/OrderServiceApplication.java
+++ Order-Service/src/main/java/com/example/amit/OrderServiceApplication.java
@@ -1,9 +1,8 @@
 package com.example.amit;
 
-import java.util.UUID;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +18,7 @@ public class OrderServiceApplication {
 	
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate()
 	{
 		return new RestTemplate();
